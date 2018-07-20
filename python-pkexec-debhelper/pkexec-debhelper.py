2547770888d747c24b62bab662d84da35298789a
Raul Rodrigo Segura <raurodse@gmail.com>
2018-07-20 09:46:57 +0200

    wip

--- python-pkexec-debhelper/pkexec-debhelper.py
+++ python-pkexec-debhelper/pkexec-debhelper.py
@@ -1,4 +1,8 @@
-import os.path
+import os
+import sys
+import json
+from jinja2 import Environment
+from jinja2.loaders import FileSystemLoader
 
 class ControlParser():
     def __init__(self):
@@ -24,13 +28,55 @@ class Debhelper():
 
 
 class PkexecDebhelper():
-    def __init__(self,debug=False):
+    def __init__(self, debhelper, debug=False):
         self.polkit_path="/usr/share/polkit-1/"
         self.polkit_path_actions = self.polkit_path + "actions"
         self.polkit_path_rules = self.polkit_path +  "rules.d"
-        self.pkexec_debhelper_path = "/usr/share/pkexec-debhelper/"
-        self.polkit_skel_file = self.pkexec_debhelper_path + "skel/polkit.skel"
+        self.tpl_env = Environment(loader=FileSystemLoader('/usr/share/pkexec-debhelper/skel'))
         self.debug = debug
+        self.debhelper = debhelper
     
     def debug_message(self, message):
         print(message)
+
+    def process_pkexec_file(self, pkg, file_to_parse):
+        with open(file_to_parse) as fd:
+            configuration = json.load(fd)
+            for package_conf in configuration:
+                if not self.generate_polkit_rules(pkg, package_conf):
+                    print('Error generating polkit files from pkexec helper')
+                    sys.exit(1)
+                if 'requiredx' in package_conf and package_conf['requiredx']:
+                    self.debhelper.add_misc_dependency(pkg, 'xpkexec')
+
+    def generate_polkit_rules(self, pkg, pkg_conf):
+        destfolder = "debian/{pkg}.polkit.action/".format(pkg)
+        pkg_conf = self.default_values_pkg_conf(pkg_conf)
+        if not pkg_conf:
+            return False
+        if not os.path.exists(destfolder):
+            os.makedirs(destfolder)
+        template = self.tpl_env.get_template('polkit.skel')
+        with open("{0}{1}.{2}".format(destfolder, pkg_conf['prefix'], pkg),'w') as fd:
+            fd.write(template.render(pkg_conf))
+        return True
+    
+    def generate_pkla(self, pkg, pkg_conf):
+        
+
+
+    def default_values_pkg_conf(self, pkg_conf):
+        if not 'cmd' in pkg_conf:
+            return False
+        if not 'prefix' in pkg_conf:
+            pkg_conf['prefix'] = "id.generic.pkexec"
+        if not 'nameaction' in pkg_conf:
+            pkg_conf['nameaction'] = 'genericnameaction'
+        if not 'icon' in pkg_conf:
+            pkg_conf['icon'] = 'noicon'
+        if not 'groups' in pkg_conf:
+            pkg_conf['groups'] = []
+        if not 'users' in pkg_conf:
+            pkg_conf['users'] = []
+        return pkg_conf
+
